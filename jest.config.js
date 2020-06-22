beec8c8267c70aa6d8ada85eadc619bccebbb0b7
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-22 20:34:58 -0300

    chore: add jest settings

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
+};
