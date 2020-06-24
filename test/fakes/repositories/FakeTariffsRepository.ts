17f12c251f41f4b047c6266193a457f49b9e3046
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-24 16:44:21 -0300

    test: update FakeTariffsRepository

--- test/fakes/repositories/FakeTariffsRepository.ts
+++ test/fakes/repositories/FakeTariffsRepository.ts
@@ -9,7 +9,7 @@ interface Tariffs {
 export default class FakeTariffsRepository implements ITariffsRepository {
   private tariffs: Tariffs[] = [
     { origin: 11, destiny: 16, tariff: 1.9 },
-    { origin: 16, destiny: 11, tariff: 2.4 },
+    { origin: 16, destiny: 11, tariff: 2.9 },
     { origin: 11, destiny: 17, tariff: 1.7 },
     { origin: 17, destiny: 11, tariff: 2.7 },
     { origin: 11, destiny: 18, tariff: 0.9 },
