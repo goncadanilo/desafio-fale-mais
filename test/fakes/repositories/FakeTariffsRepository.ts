9a01530b895a5c31eeaf76511edc8903b27e3bf9
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-06-24 14:02:19 -0300

    test: ensure that the CalculateCallCostService returns the value of the call with and without a plan

---
+++ test/fakes/repositories/FakeTariffsRepository.ts
@@ -0,0 +1,29 @@
+import ITariffsRepository from '../../../src/interfaces/ITariffsRepository';
+
+interface Tariffs {
+  origin: number;
+  destiny: number;
+  tariff: number;
+}
+
+export default class FakeTariffsRepository implements ITariffsRepository {
+  private tariffs: Tariffs[] = [
+    { origin: 11, destiny: 16, tariff: 1.9 },
+    { origin: 16, destiny: 11, tariff: 2.4 },
+    { origin: 11, destiny: 17, tariff: 1.7 },
+    { origin: 17, destiny: 11, tariff: 2.7 },
+    { origin: 11, destiny: 18, tariff: 0.9 },
+    { origin: 18, destiny: 11, tariff: 1.9 },
+  ];
+
+  public async getCallTariff(
+    origin: number,
+    destiny: number,
+  ): Promise<number | undefined> {
+    const { tariff } = this.tariffs.find(
+      key => key.origin === origin && key.destiny === destiny,
+    );
+
+    return tariff;
+  }
+}
--- test/unit/CalculateCallCostService.spec.ts
+++ test/unit/CalculateCallCostService.spec.ts
@@ -1,10 +1,15 @@
 import CalculateCallCostService from '../../src/services/CalculateCallCostService';
+import FakeTariffsRepository from '../fakes/repositories/FakeTariffsRepository';
 
 const makeCalculateCallCostService = () => {
-  const calculateCallCostService = new CalculateCallCostService();
+  const fakeTariffsRepository = new FakeTariffsRepository();
+  const calculateCallCostService = new CalculateCallCostService(
+    fakeTariffsRepository,
+  );
 
   return {
     calculateCallCostService,
+    fakeTariffsRepository,
   };
 };
 
@@ -13,5 +18,44 @@ describe('Calculate Call Cost Service', () => {
     const { calculateCallCostService } = makeCalculateCallCostService();
 
     expect(calculateCallCostService).toBeInstanceOf(CalculateCallCostService);
+  });
+
+  it('should return [0, 38] when origin: 011, destiny: 016, time: 20, plan: 30', async () => {
+    const { calculateCallCostService } = makeCalculateCallCostService();
+
+    expect(
+      await calculateCallCostService.execute({
+        origin: 11,
+        destiny: 16,
+        time: 20,
+        plan: 30,
+      }),
+    ).toStrictEqual([0, 38]);
+  });
+
+  it('should return [37.4, 136] when origin: 011, destiny: 017, time: 80, plan: 60', async () => {
+    const { calculateCallCostService } = makeCalculateCallCostService();
+
+    expect(
+      await calculateCallCostService.execute({
+        origin: 11,
+        destiny: 17,
+        time: 80,
+        plan: 60,
+      }),
+    ).toStrictEqual([37.4, 136]);
+  });
+
+  it('should return [167.2, 380] when origin: 018, destiny: 011, time: 200, plan: 120', async () => {
+    const { calculateCallCostService } = makeCalculateCallCostService();
+
+    expect(
+      await calculateCallCostService.execute({
+        origin: 18,
+        destiny: 11,
+        time: 200,
+        plan: 120,
+      }),
+    ).toStrictEqual([167.2, 380]);
   });
 });
